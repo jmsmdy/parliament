30f1186506da117eedf5073eafa2abb0031a3dd1
= <=>
2020-05-04 01:06:00 +0000

    added geometry models

--- facts/models.py
+++ facts/models.py
@@ -11,5 +11,25 @@ class Object(models.Model):
 class Person(Object):
     birthdate = models.DateField()
 
+class Point2D(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+
+    class Meta:
+        abstract = True
+
+class Polygon(models.Model):
+    coordinates = models.ArrayField(model_container=Point2D)
+
+    class Meta:
+        abstract = True
+
+class MultiPolygon(models.Model):
+    polygons = models.ArrayField(model_container=Polygon)
+
+    class Meta:
+        abstract = True
+
 class Farm(Object):
     name = models.CharField(max_length=4096)
+    location = models.EmbeddedField(model_container=MultiPolygon)
