7ea5d71ac409a22cd0134576385ffac84467f6ed
= <=>
2020-05-09 00:39:12 +0000

    testing models'

--- facts/models.py
+++ facts/models.py
@@ -18,18 +18,18 @@ class Point2D(models.Model):
     class Meta:
         abstract = True
 
-class Polygon(models.Model):
-    coordinates = models.ArrayField(model_container=Point2D)
+#class Polygon(models.Model):
+#    coordinates = models.ArrayField(model_container=Point2D)
+#
+#    class Meta:
+#        abstract = True
 
-    class Meta:
-        abstract = True
-
-class MultiPolygon(models.Model):
-    polygons = models.ArrayField(model_container=Polygon)
-
-    class Meta:
-        abstract = True
-
-class Farm(Object):
-    name = models.CharField(max_length=4096)
-    location = models.EmbeddedField(model_container=MultiPolygon)
+#class MultiPolygon(models.Model):
+#    polygons = models.ArrayField(model_container=Polygon)
+#
+#    class Meta:
+#        abstract = True
+#
+#class Farm(Object):
+#    name = models.CharField(max_length=4096)
+#    location = models.EmbeddedField(model_container=MultiPolygon)
